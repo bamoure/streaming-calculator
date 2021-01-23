3c1e3b0b4a2f6a354d68fd38654be6a3d57de37b
Barbara <barbara.reliefapps@gmail.com>
2021-01-23 19:55:32 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { ThemeService } from './services/theme.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'streaming-calculator';
+export class AppComponent implements OnInit {
+
+  public isDarkTheme: Observable<boolean>;
+
+  public deviceTypes = [
+    { label: 'Smartphone', value: 0.0012 },
+    { label: 'Tablet', value: 0.003 },
+    { label: 'Laptop', value: 0.022 },
+    { label: 'Television (50" LED)', value: 0.12 },
+  ];
+
+  public networkTypes = [
+    { label: 'WiFi', value: 0.01821 },
+    { label: '4G', value: 0.00853 }
+  ];
+
+  public totalEmissions: number;
+  public deviceConsumption: number;
+  public networkConsumption: number;
+
+  private worldValue = 463;
+
+  public form: FormGroup = new FormGroup({
+    hours: new FormControl(null, [Validators.required, Validators.min(1)]),
+    device: new FormControl(null, [Validators.required]),
+    network: new FormControl(null, [Validators.required])
+  });
+
+  constructor(private themeService: ThemeService) { }
+
+  ngOnInit(): void {
+    this.form.valueChanges.subscribe(() => {
+      if (this.form.valid) {
+        this.calculateTotalEmissions();
+      } else {
+        this.totalEmissions = null;
+      }
+    });
+    this.isDarkTheme = this.themeService.isDarkTheme;
+  }
+
+  private calculateTotalEmissions(): void {
+    this.deviceConsumption = this.form.value.hours * this.deviceValue.value;
+    this.networkConsumption = this.form.value.hours * this.networkValue.value;
+
+    // Round total emissions to two decimal places
+    this.totalEmissions = Math.round(this.worldValue * (this.deviceConsumption + this.networkConsumption) * 100) / 100;
+  }
+
+  toggleDarkTheme(checked: boolean): void {
+    this.themeService.setDarkTheme(checked);
+  }
+
+  get deviceValue(): { label: string, value: number } {
+    return this.form.get('device').value;
+  }
+
+  get networkValue(): { label: string, value: number } {
+    return this.form.get('network').value;
+  }
+
+  get hoursValue(): number {
+    return this.form.get('hours').value;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
